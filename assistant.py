1096368f3b8b524d0e6d9257e6973a9f447dc2c4
Bekhruz Niyazov <50236015+BekhruzSNiyazov@users.noreply.github.com>
2019-12-08 12:36:11 +0200

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -11,11 +11,8 @@ jokes = ["Why did the hipster burn his mouth on his coffee? Because he drank it 
 
 def speak(text):
     tts = gTTS(text=text, lang="en")
-    filename = "voice.mp3"
-    tts.save(filename)
+    playsound(tts)
     print(f"\nAssistant: {text}\n")
-    playsound(filename)
-    remove(filename)
 
 def get_audio():
     r = sr.Recognizer()
